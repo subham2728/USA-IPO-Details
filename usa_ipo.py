4f5e53638e8c29b4851c45ead6908ef97294f98f
subham2728 <subham.rouniyar27@gmail.com>
2022-03-16 13:12:08 +0545

    added email feature

--- usa_ipo.py
+++ usa_ipo.py
@@ -6,12 +6,17 @@ import datetime
 from datetime import date
 import array
 import json
+import smtplib
 
 load_dotenv()
 API_KEY = os.environ.get("API_KEY")
+SENDER_EMAIL = os.environ.get("SENDER_EMAIL")
+RECEIVER_EMAIL = os.environ.get("RECEIVER_EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
+PORT_NUMBER = os.environ.get("PORT_NUMBER")
 
 date_today = date.today()
-print("Today's date = ",date_today,type(date_today))
+print("Today's date = ",date_today)
 
 date_today_str = str(date_today)
 date_extract = datetime.datetime.strptime(date_today_str, "%Y-%m-%d")
@@ -24,7 +29,7 @@ finnhub_client = finnhub.Client(api_key=API_KEY)
 
 month_array = array.array('i',[0,31,28,31,30,31,30,31,31,30,31,30,31])
 count = 0
-days = 5
+days = 30
 
 if date_year%4==0:
     month_array[2] = 28
@@ -52,18 +57,25 @@ future_date = str(date_month)
 future_day = str(date_day)
 
 future_date = future_year + '-' + future_date + '-' + future_day
-print(future_date,type(future_date))
 
-datetime_object = datetime.datetime.strptime(future_date, "%Y-%m-%d").date()
+future_date_object = datetime.datetime.strptime(future_date, "%Y-%m-%d").date()
 
+print("Date after",days,"days = ",future_date_object)
 
-print("Date after",days,"days = ",datetime_object,type(datetime_object))
+json_date = finnhub_client.ipo_calendar(_from = "date_today", to = "2022-04-15")
 
-json_date = finnhub_client.ipo_calendar(_from = "date_today", to = "2022-3-21")
-print(type(json_date))
-# print(json_date)
+# print(type(json_date))
+
 json_formatted = json.dumps(json_date, indent=2)
-print(json_formatted)
 
+server=smtplib.SMTP('smtp.gmail.com',PORT_NUMBER)
+server.starttls()
+
+server.login(SENDER_EMAIL,PASSWORD)
+
+print("Login success")
+
+server.sendmail(SENDER_EMAIL,RECEIVER_EMAIL,json_formatted)
+print("Mail sent")
 
 
